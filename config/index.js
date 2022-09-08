01426c7265860c58e4730022c95ec9c65d68c035
sellSworder <113075741+sellSworder@users.noreply.github.com>
2022-09-08 16:58:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oRXo-5ufIN5dc1sjyzxKJOmaCUhA	",
+      id: "oRXo-5ufIN5dc1sjyzxKJOmaCUhA",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "cZ-Gritifqskj5JlluOvYuvMe4BJ2lB24uR49ICORHU",
       // 所在省份
