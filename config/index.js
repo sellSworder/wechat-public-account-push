0a03d52725c1d3cefebd15908b7557ca50c5f7d9
sellSworder <113075741+sellSworder@users.noreply.github.com>
2022-09-08 10:16:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "oRXo-5ufIN5dc1sjyzxKJOmaCUhA",
     }, 
   ],
     
